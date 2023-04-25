adc25b11f56c9bed7a3704e1a04025529032ddda
miaosiyu <miaosiyu@sensesw.onaliyun.com>
2023-04-25 10:35:57 +0800

    feat: update config

--- rollup.config.js
+++ rollup.config.js
@@ -5,40 +5,48 @@ import terser from "@rollup/plugin-terser";
 import replace from '@rollup/plugin-replace';
 import pkg from './package.json' assert { type: 'json' };
 
-export default {
-    input: 'src/index.ts',
-    output: {
-        name: 'idmeshReact',
-        file: 'dist/sdk.js',
-        format: 'umd',
-        sourcemap: true,
-        globals: {
-            'idmesh-spa-js': 'idmesh',
-            react: 'React',
-            'react-dom': 'ReactDOM'
-        }
-    },
-    external: ['react', 'react-dom', 'idmesh-spa-js'],
-    plugins: [
-        typescript({
-            allowSyntheticDefaultImports: true,
-            clean: true,
-            useTsconfigDeclarationDir: true,
-            tsconfigOverride: {
-                noEmit: false,
-                sourceMap: true,
-                compilerOptions: {
-                    lib: ['dom', 'es6']
+const getConfig = (file, format) => (
+    {
+        input: 'src/index.ts',
+        output: {
+            name: 'idmeshReact',
+            file,
+            format,
+            sourcemap: true,
+            globals: {
+                'idmesh-spa-js': 'idmesh',
+                react: 'React',
+                'react-dom': 'ReactDOM'
+            }
+        },
+        external: ['react', 'react-dom', 'idmesh-spa-js'],
+        plugins: [
+            typescript({
+                allowSyntheticDefaultImports: true,
+                clean: true,
+                useTsconfigDeclarationDir: true,
+                tsconfigOverride: {
+                    noEmit: false,
+                    sourceMap: true,
+                    compilerOptions: {
+                        lib: ['dom', 'es6']
+                    }
                 }
-            }
-        }),
-        nodeResolve({}),
-        commonjs(),
-        terser({
-            format: {
-                comments: false,
-            }
-        }),
-        replace({ __VERSION__: `'${pkg.version}'`, preventAssignment: true })
-    ],
-};
+            }),
+            nodeResolve({}),
+            commonjs(),
+            terser({
+                format: {
+                    comments: false,
+                }
+            }),
+            replace({ __VERSION__: `'${pkg.version}'`, preventAssignment: true })
+        ],
+    }
+);
+
+export default [
+    getConfig('dist/sdk.js', 'umd'),
+    getConfig(pkg.main, 'cjs'),
+    getConfig(pkg.module, 'esm'),
+];
